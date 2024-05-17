f22dfb98e77e8d33a9735c1163322414ea19473f
Luciano275 <luna800153@gmail.com>
2024-05-16 22:29:41 -0300

    Changing the background color of the add friend button

--- client/components/ui/messenger/AddFriend.tsx
+++ client/components/ui/messenger/AddFriend.tsx
@@ -2,7 +2,7 @@ import { CgAdd } from "react-icons/cg";
 
 export default function AddFriendButton() {
     return (
-        <button className="flex items-center justify-start gap-2 bg-gray-950 text-neutral-300 w-full rounded-xl py-3 px-2 hover:bg-neutral-950">
+        <button className="flex items-center justify-start gap-2 bg-gray-950 text-neutral-300 w-full rounded-xl py-3 px-2 hover:bg-green-800">
             <CgAdd size={25} />
             <span className="grow text-start">Add Friend</span>
         </button>
